5987e75a305ba971a5a8f8db759d0b1b04bdf7b6
imoran <imoran@mail.ccsf.edu>
2016-02-10 23:06:39 -0800

    added pull to refresh function

--- Twitter/TweetsViewController.swift
+++ Twitter/TweetsViewController.swift
@@ -11,26 +11,48 @@ import AFNetworking
 
 class TweetsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var tweets: [Tweet]?
-    
     @IBOutlet weak var tableView: UITableView!
     
+    var refreshControl: UIRefreshControl!
+    let delay = 3.0 * Double(NSEC_PER_SEC)
+    var tweets: [Tweet]?
+    
     override func viewDidLoad() {
-        
-            
+
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        tableView.addSubview(refreshControl)
+        
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        
     
         TwitterClient.sharedInstance.homeTimelineWithParams(nil, completion: { (tweets, error) -> () in
             self.tweets = tweets
             self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
             })
     }
   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func delay(delay:Double, closure:() -> ()) {
+        dispatch_after(
+            dispatch_time(
+              DISPATCH_TIME_NOW,
+              Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    func onRefresh() {
+        delay(1, closure: {
+            self.refreshControl.endRefreshing()
+        })
     }
     
     @IBAction func onLogout(sender: AnyObject) {
